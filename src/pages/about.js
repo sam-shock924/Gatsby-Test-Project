4ea66f6fdbdddb40762710f52860a28b2786a8cf
Sam Shock <sam.shock924@gmail.com>
2023-01-06 13:40:41 -0500

    test files

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function about() {
+	return (
+		<div>
+			<h1>About Page</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde libero,
+				nam, minus sunt quas iure tempora, qui aut cumque repellat quidem quis
+				vitae. Blanditiis commodi illo sint recusandae impedit ad.
+			</p>
+		</div>
+	);
+}
